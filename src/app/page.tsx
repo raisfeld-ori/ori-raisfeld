14c598c4174660c35c5e6c3872ed60db8c7f20af
ori raisfeld <raisfeldori@gmail.com>
2024-09-26 13:20:33 +0300

    added horizontal scroll and removed falling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,28 @@
 "use client"
-import React, { useEffect, useRef, useState } from "react";
+import React, { LegacyRef, MutableRefObject, useEffect, useRef, useState } from "react";
 import './page.css';
 import Head from "next/head";
 import Link from "next/link";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { ScrollToPlugin } from "gsap/ScrollToPlugin";
-import cs from './images/csharp.png';
 import rs from './images/rust.png';
-import qt from './images/Qt.png';
 import py from './images/python.png';
 import ts from './images/typescript.png';
 import tauri from './images/tauri.png';
 import fb from './images/firebase.png';
-import plain from './images/plain.png';
-import native from './images/react-native-logo.png';
-import cpp from './images/cpp.png';
-import c from './images/c.png';
-import tf from './images/tf.png';
-import unity from './images/unity.png';
-import sql from './images/sql.png';
 import expo from './images/expo.png';
 import react from './images/React.png';
+import ksed from './images/example.png';
 import me from './images/me.jpg';
+import hibori from './images/Hibori.jpg';
+import hfcig from './images/logo.png';
+import d_vault from './images/ori.png';
+import chamama from './images/chamama.png';
+import openuni from './images/openuni.png';
+import tegreai from './images/tegreai.jpeg';
 import arrow from './images/arrow-down-svgrepo-com.svg';
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 
 gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
@@ -35,15 +33,11 @@ export default function Home() {
     "I'm a fullstack developer",
     "extra"
   ];
-  let random = 1;
-  const images = [
-    cs, rs, qt, py, ts, tauri, fb, plain, native, cpp,
-    c, tf, unity, sql, expo, react,
-  ];
   const [part, set_part] = useState(-1);
   const [text, set_text] = useState("");
   const [started, set_started] = useState(false);
-  const start = false;
+  const projects_ref = useRef<HTMLDivElement>(null);
+  const start = true;
   useEffect(() => {
     setTimeout(() => {
       if (start){set_started(true);}
@@ -70,20 +64,48 @@ export default function Home() {
     }, 1500);
   }, [part]);
   useEffect(() => {
-    random = Math.floor(Math.random() * images.length);
-    let tl = gsap.timeline();
-    tl.to(".falling_img", {
+    if (!started){return}
+    gsap.to(".pop_up", {
       scrollTrigger: {
-        trigger: ".falling",
-        start: "top top",
-        end: "bottom center",
-        scrub: true,
+        trigger: ".pop_up",
+        start: "top center",
+        end: "top center",
+        onEnter: () => {
+          projects_ref.current?.classList.add("focus");
+        },
+        onEnterBack: () => {
+          projects_ref.current?.classList.remove("focus");
+        }
       },
-      duration: 100,
-      y: "110svh",
-      rotate: 720,
     });
-  }, []);
+    gsap.to(".pop_up", {
+      scrollTrigger: {
+        trigger: ".pop_up",
+        start: "bottom center",
+        end: "bottom center",
+        onEnter: () => {
+          projects_ref.current?.classList.remove("focus");
+        },
+        onEnterBack: () => {
+          projects_ref.current?.classList.add("focus");
+        }
+      },
+    });
+    gsap.to(".scroll", {
+      scrollTrigger: {
+        trigger: ".scroll",
+        start: "top 20%",
+        end: "+=700svw",
+        pin: true,
+        scrub: 1,
+        markers: true,
+      },
+      duration: 3,
+      ease: "none",
+      x: "-310svw",
+      y: 0,
+    })
+  }, [started]);
   return (
     <div className="h-full overflow-x-hidden overflow-y-hidden">
     <nav className="menu border-white border-b-2" style={{transform: started ? "translateY(0)" : "translateY(-100%)"}}>
@@ -108,27 +130,6 @@ export default function Home() {
           <div className="background" style={{opacity: started ? 1 : 0}}></div>
           <div className="arrow" style={{opacity: started ? 1 : 0}}>🡣</div>
         </section>
-        <section className="falling" style={{display: started ? "" : "none"}}>
-          <>
-            {images.map((image, i) => {
-            return <Image src={image} key={i} alt="" loading="lazy" className={"falling_img location" + i}
-              ></Image>
-            })}
-            {[...Array(4)].map((_, i) => {
-              return <>
-              <div key={i + "line"} className="line" style={{
-                bottom: (-25 * i) - 20 + 3 + "svh", left: (2 * (i * random) + 1) + "svw"
-              }}></div>
-                <div key={i + "line"} className="line" style={{
-                bottom: (-25 * i) - 20 + 5 + "svh", right: (2 * i + 5) + "svw"
-              }}></div>
-                <div key={i + "line"} className="line" style={{
-                bottom: (-25 * i) - 20 + 9 + "svh", right: (2 * (i * random) + 5) + 40 + "svw"
-              }}></div>
-              </>
-            })}
-          </>
-        </section>
         <section className="skills" style={{display: started ? "" : "none"}}>
             <h2 className="title text-center">My skills</h2>
             <div style={{marginTop: "100svh"}}>
@@ -143,14 +144,6 @@ export default function Home() {
               <Image src={ts} alt="" className="logo"></Image>
             </ul>
             </div>
-            <h3 className="text-center text-6xl mb-5">Other Tools:</h3>
-            <ul className="flex justify-center flex-row">
-            <Image src={cs} alt="" className="logo"></Image>
-              <Image src={unity} alt="" className="logo"></Image>
-              <Image src={tf} alt="" className="logo"></Image>
-              <Image src={qt} alt="" className="logo"></Image>
-              <Image src={plain} alt="" className="logo"></Image>
-            </ul>
         </section>
         <section className="about_me m-1" style={{display: started ? "" : "none"}}>
           <h1 className="title text-center">About me</h1>
@@ -161,14 +154,68 @@ export default function Home() {
               I started with 0 knowlage, by simply writing "python" into youtube during school,
               while creating my first application, I learned how fun and difficult can creating apps.
               <br></br><br></br>
-              I am currently studying at Chamama Highschool and trying to get a B.C in The open University.
+              I am currently studying at Chamama Highschool and trying to get a B.A in The open University.
             </p>
             <Image src={me} className="me" alt=""></Image>
           </div>
         </section>
-        <section className="projects">
-
+        <section className="projects" style={{display: started ? "" : "none"}}>
+            <div className="pop_up" ref={projects_ref}>
+                <div className="line">
+                  <Project name="Ksed" height={17} width={17} image={ksed}></Project>
+                  <Project name="HFCIG" height={12} width={17} image={hfcig}></Project>
+                </div>
+                <div className="line">
+                  <Project name="Hibori" height={15} width={7} image={hibori}></Project>
+                  <Project name="D vault" height={17} width={17} image={d_vault}></Project>
+                </div>
+            </div>
+        </section>
+        <section className="jobs" style={{display: started ? "" : "none"}}>
+          <h1 className="title">Jobs / Education:</h1>
+          <div className="scroll">
+            <div className="job">
+              <div>
+              <h2 className="job_title">Chamama Highschool</h2>
+              <h3 className="achivement">2021 - Present</h3>
+              <p className="job_description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id dolorem at quisquam reiciendis adipisci. Quia ullam excepturi laborum nostrum nam, aspernatur aliquam delectus in doloremque natus culpa unde a enim.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste repudiandae nostrum dolorem, asperiores voluptate veritatis debitis. Dicta odio impedit tenetur possimus vero ex commodi in deleniti, rerum provident, ut aliquam?
+              </p>
+              </div>
+              <Image className="job_logo" src={chamama} alt=""></Image>
+            </div>
+            <div className="job">
+              <div>
+              <h2 className="job_title">The Open University</h2>
+              <h3 className="achivement">2022 - Present</h3>
+              <p className="job_description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id dolorem at quisquam reiciendis adipisci. Quia ullam excepturi laborum nostrum nam, aspernatur aliquam delectus in doloremque natus culpa unde a enim.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste repudiandae nostrum dolorem, asperiores voluptate veritatis debitis. Dicta odio impedit tenetur possimus vero ex commodi in deleniti, rerum provident, ut aliquam?
+              </p>
+              </div>
+              <Image className="job_logo" src={openuni} alt=""></Image>
+            </div>
+            <div className="job">
+              <div>
+              <h2 className="job_title">TeGriAi</h2>
+              <h3 className="achivement">2024 - Present</h3>
+              <p className="job_description">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id dolorem at quisquam reiciendis adipisci. Quia ullam excepturi laborum nostrum nam, aspernatur aliquam delectus in doloremque natus culpa unde a enim.
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste repudiandae nostrum dolorem, asperiores voluptate veritatis debitis. Dicta odio impedit tenetur possimus vero ex commodi in deleniti, rerum provident, ut aliquam?
+              </p>
+              </div>
+              <Image className="job_logo" src={tegreai} alt=""></Image>
+            </div>
+          </div>
         </section>
     </div>
   );
+}
+
+function Project(props: {name: string, image: StaticImageData, height: number, width: number}) {
+  return <div className="project" style={{height: props.height + "em", width: props.width + "em"}} id={props.name}>
+      <div className="project_image"><Image src={props.image} alt={props.name}></Image></div>
+      <div className="project_text">{props.name}</div>
+    </div>
 }

3fef8785d081a249cac242c2d11f95583084e6c8
ori raisfeld <raisfeldori@gmail.com>
2024-09-27 14:44:17 +0300

    fixed skills and made the website mobile responsive

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, { LegacyRef, MutableRefObject, useEffect, useRef, useState } from "react";
+import React, { LegacyRef, MutableRefObject, RefObject, useEffect, useRef, useState } from "react";
 import './page.css';
 import Head from "next/head";
 import Link from "next/link";
@@ -21,6 +21,9 @@ import d_vault from './images/ori.png';
 import chamama from './images/chamama.png';
 import openuni from './images/openuni.png';
 import tegreai from './images/tegreai.jpeg';
+import qt from './images/qt.png';
+import google_pdf from './images/screenshot.png';
+import rds from './images/light.png';
 import arrow from './images/arrow-down-svgrepo-com.svg';
 import Image, { StaticImageData } from "next/image";
 
@@ -37,7 +40,13 @@ export default function Home() {
   const [text, set_text] = useState("");
   const [started, set_started] = useState(false);
   const projects_ref = useRef<HTMLDivElement>(null);
-  const start = true;
+  const about_ref = useRef<HTMLDivElement>(null);
+  const jobs_ref = useRef<HTMLDivElement>(null);
+  const scroll_to = (ref: RefObject<HTMLDivElement>) => {
+    if (!ref.current){return}
+    window.scrollTo({top: ref.current.offsetTop, behavior: "smooth"});
+  }
+  const start = false;
   useEffect(() => {
     setTimeout(() => {
       if (start){set_started(true);}
@@ -94,11 +103,10 @@ export default function Home() {
     gsap.to(".scroll", {
       scrollTrigger: {
         trigger: ".scroll",
-        start: "top 20%",
-        end: "+=700svw",
+        start: "top 30%",
+        end: "+=900svw",
         pin: true,
         scrub: 1,
-        markers: true,
       },
       duration: 3,
       ease: "none",
@@ -117,10 +125,10 @@ export default function Home() {
             </Link>
           </div>
           <div className="flex items-center">
-          <Link href={""} className="m-5 hover:text-neutral-700">Projects</Link>
-          <Link href={""} className="m-5 hover:text-neutral-700">Jobs</Link>
-          <Link href={""} className="m-5 hover:text-neutral-700">Skills</Link>
-          <Link href={""} className="m-5 hover:text-neutral-700">Resume</Link>
+          <div onClick={() => scroll_to(projects_ref)} className="m-5 cursor-pointer hover:text-neutral-700">Projects</div>
+          <div onClick={() => scroll_to(jobs_ref)} className="m-5 cursor-pointer hover:text-neutral-700">Jobs</div>
+          <div onClick={() => scroll_to(about_ref)} className="m-5 cursor-pointer hover:text-neutral-700">About</div>
+          <div onClick={() => {}} className="m-5 cursor-pointer hover:text-neutral-700">Resume</div>
           </div>
         </div>
       </div>
@@ -130,24 +138,9 @@ export default function Home() {
           <div className="background" style={{opacity: started ? 1 : 0}}></div>
           <div className="arrow" style={{opacity: started ? 1 : 0}}>🡣</div>
         </section>
-        <section className="skills" style={{display: started ? "" : "none"}}>
-            <h2 className="title text-center">My skills</h2>
-            <div style={{marginTop: "100svh"}}>
-            <h3 className="text-center text-6xl mb-5">Main Tools:</h3>
-            <ul className="flex justify-center flex-row mb-10">
-              <Image src={py} alt="" className="logo"></Image>
-              <Image src={rs} alt="" className="logo"></Image>
-              <Image src={expo} alt="" className="logo"></Image>
-              <Image src={tauri} alt="" className="logo"></Image>
-              <Image src={react} alt="" className="logo"></Image>
-              <Image src={fb} alt="" className="logo"></Image>
-              <Image src={ts} alt="" className="logo"></Image>
-            </ul>
-            </div>
-        </section>
-        <section className="about_me m-1" style={{display: started ? "" : "none"}}>
+        <section className="about_me m-1" ref={about_ref} style={{display: started ? "" : "none"}}>
           <h1 className="title text-center">About me</h1>
-          <div className="flex flex-row justify-center items-center m-2">
+          <div className="flex about flex-row justify-center items-center m-2">
             <p className="me_text">
               I'm Ori Raisfeld, A fullstack developer, I started programming 3 years ago in 2021.
               <br /><br />
@@ -159,19 +152,39 @@ export default function Home() {
             <Image src={me} className="me" alt=""></Image>
           </div>
         </section>
+        <section className="skills" style={{display: started ? "" : "none"}}>
+            <h2 className="title text-center">My skills</h2>
+            <h3 className="text-center text-6xl mb-5">My Languages:</h3>
+            <ul className="flex justify-center flex-row mb-10">
+              <Image src={py} alt="" className="logo"></Image>
+              <Image src={rs} alt="" className="logo"></Image>
+              <Image src={react} alt="" className="logo"></Image>
+              <Image src={ts} alt="" className="logo"></Image>
+            </ul>
+            <h3 className="text-center text-6xl mb-5">My Tools:</h3>
+            <ul className="flex justify-center flex-row">
+              <Image src={tauri} alt="" className="logo"></Image>
+              <Image src={expo} alt="" className="logo" style={{width: "4em"}}></Image>
+              <Image src={fb} alt="" className="logo"></Image>
+              <Image src={qt} alt="" className="logo"></Image>
+            </ul>
+        </section>
         <section className="projects" style={{display: started ? "" : "none"}}>
+          <h1 className="title text-center">Projects:</h1>
             <div className="pop_up" ref={projects_ref}>
                 <div className="line">
                   <Project name="Ksed" height={17} width={17} image={ksed}></Project>
                   <Project name="HFCIG" height={12} width={17} image={hfcig}></Project>
+                  <Project name="RDS" height={12} width={17} image={rds}></Project>
                 </div>
                 <div className="line">
-                  <Project name="Hibori" height={15} width={7} image={hibori}></Project>
                   <Project name="D vault" height={17} width={17} image={d_vault}></Project>
+                  <Project name="Hibori" height={15} width={7} image={hibori}></Project>
+                  <Project name="Google PDF" height={12} width={12} image={google_pdf}></Project>
                 </div>
             </div>
         </section>
-        <section className="jobs" style={{display: started ? "" : "none"}}>
+        <section className="jobs" ref={jobs_ref} style={{display: started ? "" : "none"}}>
           <h1 className="title">Jobs / Education:</h1>
           <div className="scroll">
             <div className="job">
@@ -179,8 +192,7 @@ export default function Home() {
               <h2 className="job_title">Chamama Highschool</h2>
               <h3 className="achivement">2021 - Present</h3>
               <p className="job_description">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id dolorem at quisquam reiciendis adipisci. Quia ullam excepturi laborum nostrum nam, aspernatur aliquam delectus in doloremque natus culpa unde a enim.
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste repudiandae nostrum dolorem, asperiores voluptate veritatis debitis. Dicta odio impedit tenetur possimus vero ex commodi in deleniti, rerum provident, ut aliquam?
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id dolorem at quisquam reiciendis adipisci. Quia ullam excepturi laborum nostrum nam, aspernatur aliquam delectus in doloremque natus culpa unde a enim
               </p>
               </div>
               <Image className="job_logo" src={chamama} alt=""></Image>
@@ -191,7 +203,6 @@ export default function Home() {
               <h3 className="achivement">2022 - Present</h3>
               <p className="job_description">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id dolorem at quisquam reiciendis adipisci. Quia ullam excepturi laborum nostrum nam, aspernatur aliquam delectus in doloremque natus culpa unde a enim.
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste repudiandae nostrum dolorem, asperiores voluptate veritatis debitis. Dicta odio impedit tenetur possimus vero ex commodi in deleniti, rerum provident, ut aliquam?
               </p>
               </div>
               <Image className="job_logo" src={openuni} alt=""></Image>
@@ -202,11 +213,15 @@ export default function Home() {
               <h3 className="achivement">2024 - Present</h3>
               <p className="job_description">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Id dolorem at quisquam reiciendis adipisci. Quia ullam excepturi laborum nostrum nam, aspernatur aliquam delectus in doloremque natus culpa unde a enim.
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste repudiandae nostrum dolorem, asperiores voluptate veritatis debitis. Dicta odio impedit tenetur possimus vero ex commodi in deleniti, rerum provident, ut aliquam?
               </p>
               </div>
               <Image className="job_logo" src={tegreai} alt=""></Image>
             </div>
+          </div>
+        </section>
+        <section className="contact">
+          <h1 className="title">Contact Me!</h1>
+          <div className="contact_form">
           </div>
         </section>
     </div>

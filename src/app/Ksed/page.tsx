5bc94a9f7843cf2d6c825711f0157b9cfa8b87dc
ori raisfeld <raisfeldori@gmail.com>
2024-09-28 19:21:16 +0300

    working on the project page

--- src/app/Ksed/page.tsx
+++ src/app/Ksed/page.tsx
@@ -1,6 +1,12 @@
+'use client'
+import Image from 'next/image';
+import arrow_left from '../images/arrow-left-direction-icon.svg';
+import { useRouter } from 'next/navigation';
+
 export default function Page(){
+    const navigation = useRouter();
     return <div className="hero">
         <div className="background"></div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione dolor a est corrupti odit. Ullam iure porro deserunt necessitatibus quidem labore laborum vero voluptatibus sint quas nobis, molestias odio doloribus.
+        <div className="arrow-back" onClick={() => navigation.back()}><Image alt="" src={arrow_left}></Image></div>
     </div>
 }
